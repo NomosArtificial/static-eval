e72010eedae9466c736bbeec69eec3c89daf5b8c
OrestesK <oresteskassian@gmail.com>
2023-06-25 08:41:27 -0400

    Added correct label_col

--- static-eval/eval_llm.py
+++ static-eval/eval_llm.py
@@ -6,16 +6,13 @@ import asyncio
 import torch
 from typing import Iterable, Dict
 from tqdm import tqdm
-from dataclasses import dataclass 
+from dataclasses import dataclass
 from langchain.llms.base import BaseLLM
 from langchain.llms import OpenAI, Anthropic
 from langchain.chains import LLMChain
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    HumanMessagePromptTemplate
-)
+from langchain.prompts.chat import ChatPromptTemplate, HumanMessagePromptTemplate
 from accelerate import init_empty_weights, load_checkpoint_and_dispatch
 from datasets import load_dataset
 from huggingface_hub import hf_hub_download
@@ -39,7 +36,7 @@ SUPPORTED_MODELS = [
     "text-davinci-003",
     "claude-instant-v1",
     "claude-v1.2",
-    "claude-v1.3"
+    "claude-v1.3",
 ]
 
 RATE_LIMITS = {
@@ -49,7 +46,7 @@ RATE_LIMITS = {
     "text-davinci-003": 3250,
     "claude-instant-v1": None,
     "claude-v1.2": None,
-    "claude-v1.3": None
+    "claude-v1.3": None,
 }
 
 
@@ -60,6 +57,8 @@ Wrapper for running LangChain models on a set of inputs with a simplified interf
 * Backoff/retry for failed requests
 * Unified template interface for Chat and vanilla LLMs based on a single string template
 """
+
+
 class EvalLLM:
     def __init__(self, config: LLMConfig, template_string: str):
         self.config = config
@@ -70,13 +69,16 @@ class EvalLLM:
         self.template_string = template_string
         self.is_chat = config.model_name in ["gpt-3.5-turbo", "gpt-4"]
         self.apply_async = config.model_name in [
-            "gpt-3.5-turbo", "gpt-4", "text-davinci-002", "text-davinci-003"
+            "gpt-3.5-turbo",
+            "gpt-4",
+            "text-davinci-002",
+            "text-davinci-003",
         ]
         base_prompt = PromptTemplate(
-            template=template_string, 
-            template_format="jinja2", 
-            input_variables=["document"], 
-            validate_template=False
+            template=template_string,
+            template_format="jinja2",
+            input_variables=["document"],
+            validate_template=False,
         )
         prompt = None
 
@@ -89,14 +91,28 @@ class EvalLLM:
         model = None
 
         if config.model_name in ["gpt-3.5-turbo", "gpt-4"]:
-            model = ChatOpenAI(model_name=config.model_name, max_tokens=config.max_new_tokens, temperature=config.temperature)
+            model = ChatOpenAI(
+                model_name=config.model_name,
+                max_tokens=config.max_new_tokens,
+                temperature=config.temperature,
+            )
         elif config.model_name in ["claude-instant-v1", "claude-v1.2", "claude-v1.3"]:
-            model = Anthropic(model=config.model_name, max_tokens_to_sample=config.max_new_tokens, temperature=config.temperature)
+            model = Anthropic(
+                model=config.model_name,
+                max_tokens_to_sample=config.max_new_tokens,
+                temperature=config.temperature,
+            )
         elif config.model_name in ["text-davinci-002", "text-davinci-003"]:
-            model = OpenAI(model_name=config.model_name, max_tokens=config.max_new_tokens, temperature=config.temperature)
+            model = OpenAI(
+                model_name=config.model_name,
+                max_tokens=config.max_new_tokens,
+                temperature=config.temperature,
+            )
         elif "LawInformedAI/" in config.model_name:
             checkpoint = config.model_name
-            weights_location = hf_hub_download(checkpoint, "pytorch_model.bin.index.json")
+            weights_location = hf_hub_download(
+                checkpoint, "pytorch_model.bin.index.json"
+            )
             tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 
             with init_empty_weights():
@@ -111,10 +127,15 @@ class EvalLLM:
                 model=model,
                 checkpoint=weights_location,
                 dtype=torch.bfloat16,
-                device_map="auto"
+                device_map="auto",
             )
-            model_kwargs = {"temperature": config.temperature, "max_new_tokens": config.max_new_tokens}
-            model = CustomHFModel(model=model, tokenizer=tokenizer, generate_kwargs=model_kwargs)
+            model_kwargs = {
+                "temperature": config.temperature,
+                "max_new_tokens": config.max_new_tokens,
+            }
+            model = CustomHFModel(
+                model=model, tokenizer=tokenizer, generate_kwargs=model_kwargs
+            )
             self.recommended_chunk_size = 4
         self.chain = LLMChain(llm=model, prompt=prompt)
 
@@ -122,36 +143,54 @@ class EvalLLM:
         start = time.time()
         completions = []
         chain_input = [{"document": input} for input in inputs]
-        print("Generating completions with rate limit of {} calls per minute".format(self.config.rate_limit))
-        for chunk_idx, chunk in enumerate(tqdm(chunked_iterable(chain_input, chunk_size), total=math.ceil(len(chain_input) / chunk_size))):
+        print(
+            "Generating completions with rate limit of {} calls per minute".format(
+                self.config.rate_limit
+            )
+        )
+        for chunk_idx, chunk in enumerate(
+            tqdm(
+                chunked_iterable(chain_input, chunk_size),
+                total=math.ceil(len(chain_input) / chunk_size),
+            )
+        ):
             if self.apply_async:
                 completions += await self.chain.aapply(chunk)
-            else: 
+            else:
                 completions += self.chain.apply(chunk)
 
             if self.config.rate_limit is None:
                 continue
-            # check if we are over the rate limit  
+            # check if we are over the rate limit
             elapsed = time.time() - start
             total_calls = (chunk_idx + 1) * (chunk_size + 1)
             expected_time = total_calls * 1 / self.config.rate_limit * 60
             await asyncio.sleep(0.5)
-            if elapsed < expected_time + 15 and chunk_idx < math.ceil(len(chain_input) / chunk_size) - 1:
+            if (
+                elapsed < expected_time + 15
+                and chunk_idx < math.ceil(len(chain_input) / chunk_size) - 1
+            ):
                 # wait for elapsed time to catch up with expected time for current number of calls
                 await asyncio.sleep(expected_time - elapsed + 15)
-        print("Finished generating completions in {:.2f} seconds".format(time.time() - start))
+        print(
+            "Finished generating completions in {:.2f} seconds".format(
+                time.time() - start
+            )
+        )
         return completions
 
+
 def try_parse(completion):
-  try:
-    result = json.loads(completion)
-    return result["answer"]
-  except:
-    return ""
+    try:
+        result = json.loads(completion)
+        return result["answer"]
+    except:
+        return ""
+
 
 async def test_eval_llm():
     questions = list(load_dataset("andersonbcdefg/mpre", split="train"))
-    answers = ['abcd'[q['correct_idx']] for q in questions]
+    answers = ["abcd"[q["correct_idx"]] for q in questions]
     template_string = """You are a legal expert who specializes in professional ethics. \
 In response to the following multiple-choice question about legal professional ethics, please identify the correct answer. \
 Your response should be in JSON with a single key, "answer", and a value of "a", "b", "c", or "d".
@@ -164,14 +203,22 @@ b. {{ document.candidate_answers[1] }}
 c. {{ document.candidate_answers[2] }}
 d. {{ document.candidate_answers[3] }}
 
-ANSWER:""" 
+ANSWER:"""
 
-    for model_name, rate_limit in zip(SUPPORTED_MODELS, [3500, 200, 3500, 3500, None, None]):
-        config = LLMConfig(model_name=model_name, max_new_tokens=20, rate_limit=rate_limit)
+    for model_name, rate_limit in zip(
+        SUPPORTED_MODELS, [3500, 200, 3500, 3500, None, None]
+    ):
+        config = LLMConfig(
+            model_name=model_name, max_new_tokens=20, rate_limit=rate_limit
+        )
         llm = EvalLLM(config, template_string)
         completions = await llm.run(questions)
-        preds = [try_parse(c['text']) for c in completions]
-        print(f"{model_name} Accuracy:", sum([p == a for p, a in zip(preds, answers)]) / len(answers))
+        preds = [try_parse(c["text"]) for c in completions]
+        print(
+            f"{model_name} Accuracy:",
+            sum([p == a for p, a in zip(preds, answers)]) / len(answers),
+        )
+
 
 if __name__ == "__main__":
     fire.Fire(test_eval_llm)
--- static-eval/main.py
+++ static-eval/main.py
@@ -6,9 +6,12 @@ import pandas as pd
 # get current working directory
 os.getcwd()
 import sys
+
 sys.path.append("..")
 from legalbench import LBTask
 from eval_llm import EvalLLM, LLMConfig
+
+
 async def main():
     os.environ["OPENAI_API_KEY"] = "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"
     task_dir = "legalbench"
@@ -18,28 +21,34 @@ async def main():
         for name in os.listdir(task_dir)
         if os.path.isdir(os.path.join(task_dir, name))
     ]
+    tasks = tasks[11:17]
+
     # creates a file to store the results in
     header = True
     results_filename = str(datetime.now()) + "_RESULTS.csv"
     open(results_filename, "w+").close()
 
     for task_name in tasks:
-
-        task = LBTask(task_name)
+        task = LBTask(task_name, label_col="answer")
         data = task.prepare_data()
-        config = LLMConfig(model_name="gpt-3.5-turbo", max_new_tokens=20, temperature=0.0, rate_limit=3500)
+        config = LLMConfig(
+            model_name="gpt-3.5-turbo",
+            max_new_tokens=6,
+            temperature=0.0,
+            rate_limit=600,
+        )
         llm = EvalLLM(config, task.prompt_template())
         completions = await llm.run(data)
         metrics = task.score(data, completions)
         print(metrics)
         # append task result to result_file
-        results_df = pd.DataFrame(
-            {"test": [task_name], "metrics": [metrics]}
-        )
+        results_df = pd.DataFrame({"test": [task_name], "metrics": [metrics]})
 
         results_df.to_csv(results_filename, mode="a", index=False, header=header)
         header = False
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import asyncio
+
     asyncio.run(main())
